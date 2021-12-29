8e85ee6818f8866d7f42673280556a1542095eb0
eliTheDevGuy <EArmendariz000@gmail.com>
2021-12-28 19:01:28 -0800

    Begun function showPage
    Began work on the showPage function by creating elements needed to display the objects from the data array variable and appending them to the ul.

--- js/script.js
+++ js/script.js
@@ -11,13 +11,39 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
+const itemsPerPage = 9 ;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
+function showPage ( list , page ) {
+   const startIndex = ( page * itemsPerPage ) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   const ul = document.querySelector('.student-list');
+   ul.innerHTML = `` ;
+   for ( i = 0 ; i >= startIndex && i < endIndex ; i++ ) {
+      const li = document.createElement('LI');
+      li.className = 'student-item cf';
+      ul.appendChild(li);
+      const studentDiv = document.createElement('DIV');
+      studentDiv.className = 'student-details';
+      li.appendChild(studentDiv);
+      const img = document.createElement('IMG');
+      img.className = 'avatar';
+      img.src = `${data[i].picture.medium}`;
+        /* <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+               <h3>Ethel Dean</h3>
+               <span class="email">ethel.dean@example.com</span>
+             </div>
+             <div class="joined-details">
+               <span class="date">Joined 12-15-2005</span>
+            </div>
+         </li>*/
+   } 
+}
 
 
 /*
