bac6ae1ed1b56874d1e5601fc0b6ad3d02214a43
eliTheDevGuy <EArmendariz000@gmail.com>
2021-12-29 17:42:43 -0800

    Finished showPage function
    Finished the function to create and append user data blocks to an li which loops through all users and appends the li's to a running ul. Also changed itemsPerPage to 10 to be more aesthetically pleasing!

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-const itemsPerPage = 9 ;
+const itemsPerPage = 10 ;
 
 /*
 Create the `showPage` function
@@ -30,21 +30,39 @@ function showPage ( list , page ) {
       studentDiv.className = 'student-details';
       li.appendChild(studentDiv);
       const img = document.createElement('IMG');
+      let avatar = data[i].picture.large;
       img.className = 'avatar';
-      img.src = `${data[i].picture.medium}`;
+      img.src = `${avatar}`;
       img.alt = `Profile Picture`;
       studentDiv.appendChild(img);
       const h3 = document.createElement('H3');
+      let fullName = data[i].name.title;
+      fullName += ' ';
+      fullName += data[i].name.first;
+      fullName += ' ';
+      fullName += data[i].name.last;
+      h3.textContent = fullName;
       studentDiv.appendChild(h3);
-      const span = document.createElement('SPAN');
-      span.className = 'email';
-        /* <li class="student-item cf">
+      const emailSpan = document.createElement('SPAN');
+      emailSpan.className = 'email';
+      const emailAddr = data[i].email;
+      emailSpan.textContent = emailAddr;
+      studentDiv.appendChild(emailSpan);
+      const joinedDiv = document.createElement('DIV');
+      joinedDiv.className = 'date';
+      let joinDate = data[i].registered.date;
+      joinedDiv.textContent = `Joined ${joinDate}`;
+      li.appendChild(joinedDiv);
+      ul.insertAdjacentElement( "beforeend" , li );
+
+        /* DYNAMICALLY CREATES THIS LI STRUCTURE ON USER DATA
+         <li class="student-item cf">
             <div class="student-details">
                <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
                <h3>Ethel Dean</h3>
                <span class="email">ethel.dean@example.com</span>
-             </div>
-             <div class="joined-details">
+            </div>
+            <div class="joined-details">
                <span class="date">Joined 12-15-2005</span>
             </div>
          </li>*/
@@ -60,3 +78,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage (data,all);
